62c9f1d7b2d6c31edb6d1eb1e604ac7d870e59b0
mayanku1111 <138918191+mayanku1111@users.noreply.github.com>
2025-03-26 02:18:08 +0530

    Update route.ts

--- polymind/src/app/api/generate-questions/route.ts
+++ polymind/src/app/api/generate-questions/route.ts
@@ -1,10 +1,36 @@
-import next from "next";
+import { generateText } from 'ai';
 import { NextResponse } from "next/server";
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+
+
+const openrouter = createOpenRouter({
+    apiKey: process.env.OPENROUTER_API_KEY,
+  });
+
+
+const clarifyResearchGoals = async (topic: string) => {
+
+    try{
+        const { text } = await generateText({
+            model: yourModel,
+            prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+});
+        
+    }catch{
+
+    }
+}
 
 export async function POST(req: Request) {
 
     const {topic} = await req.json();
     console.log("Topic:" ,topic);
+
+    try{
+
+    }catch{
+
+    }
 
     return NextResponse.json({
         success:true
