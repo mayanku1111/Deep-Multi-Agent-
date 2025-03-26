63f4ab3cb94d0db047ef1e724fdeedf5f450b7f8
mayanku1111 <138918191+mayanku1111@users.noreply.github.com>
2025-03-27 02:25:23 +0530

    Update route.ts

--- polymind/src/app/api/generate-questions/route.ts
+++ polymind/src/app/api/generate-questions/route.ts
@@ -1,12 +1,12 @@
-import { generateText } from 'ai';
 import { NextResponse } from "next/server";
+import { generateObject } from 'ai';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { z } from "zod";
 
 
 const openrouter = createOpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY || "",
   });
-
 
 const clarifyResearchGoals = async (topic: string) => {
 
@@ -37,42 +37,43 @@ FORMAT YOUR RESPONSE AS:
 Be thoughtful and specific - these questions will guide the entire research process.
 `
 
+
     try{
-        const { text } = await generateText({
-            model: openrouter("google/gemini-2.5-pro-exp-03-25:free"),
+        const { object } = await generateObject({
+            model: openrouter("meta-llama/llama-3.3-70b-instruct"),
             prompt,
-        });
-        return text;
+            schema: z.object({
+                questions: z.array(z.string())
+            })
+          });
 
-        
+          return object.questions;
     }catch(error){
-        console.log("Error generating questions:" ,error)
-
+    console.log("Error while generating questions: ", error)
 
     }
 }
 
-export async function POST(req: Request) {
+
+export async function POST(req: Request){
 
     const {topic} = await req.json();
-    console.log("Topic:" ,topic);
+    console.log("Topic: ", topic);
 
     try{
+           const questions = await clarifyResearchGoals(topic);
+           console.log("Questions: ", questions)
 
-        const questions = await clarifyResearchGoals(topic);
-        console.log("Questions:" ,questions);
-
-        return NextResponse.json({
-            success:true,
-            questions
-        }, {status:200})
-
+           return NextResponse.json(questions)
     }catch(error){
-        console.error("Error generating questions:", error);
+
+       console.error("Error while generating questions: ", error)
         return NextResponse.json({
-            success:false,
-            error: "Error generating questions"
-        }, {status:500});
+            success: false, error: "Failed to generate questions"
+        }, {status: 500})
 
     }
+
+
+   
 }
